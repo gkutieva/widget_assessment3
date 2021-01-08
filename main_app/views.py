43b93ac2508754575a25f1f5a469fb8aeb168ece
gkutieva <73370477+gkutieva@users.noreply.github.com>
2021-01-08 14:25:17 -0700

    Complete Step 3.4

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
+from .forms import WidgetForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
 def index(request):
     widgets = Widget.objects.all()
     return render(request, 'index.html', { 'widgets': widgets })
+
+class WidgetCreate(CreateView):
+    model = Widget
+    fields = "__all__"    
+
+def delete_widget(request, widget_id):
+    Widget.objects.get(id=widget_id).delete()
+    return redirect('/')
