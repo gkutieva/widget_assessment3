17276e52296d9571306da77647ba118cbb76a3d3
gkutieva <73370477+gkutieva@users.noreply.github.com>
2021-01-08 12:37:38 -0700

    Complete Step 3.1

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
-class Widget(models.Model):
-    
+class Widget(models.Model):    
+    item = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.item
+
+    def get_absolute_url(self):
+        return reverse('index')
