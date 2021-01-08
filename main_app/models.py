77293fdc77bc00184104a4c213c25cdfe9594a8e
gkutieva <73370477+gkutieva@users.noreply.github.com>
2021-01-08 13:03:50 -0700

    Complete Step 3.2

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 
 # Create your models here.
 class Widget(models.Model):    
-    item = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    quantity = models.IntegerField()
 
     def __str__(self):
         return self.item
